55c77db6f862939281293d286aecc6a72ddd582b
daniellesilveira-ds <74359213+daniellesilveira-ds@users.noreply.github.com>
2020-11-19 17:43:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL="https://https://java-spring-danielle.herokuapp.com/validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
